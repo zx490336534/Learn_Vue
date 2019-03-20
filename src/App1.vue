ae876510cdcbd8708609a6cfd2b1289dfc0551a3
zx490336534 <490336534@qq.com>
2019-03-20 23:45:01 +0800

    新建App1，使用分离的方式完成上一个提交的单html的内容

---
+++ src/App1.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+        {{message}}
+        <ol>
+            <todo-item v-for="item in groceryList" v-bind:todo="item" v-bind:key="item.id"></todo-item>
+        </ol>
+    </div>
+</template>
+
+<script>
+    import TodoItem from './components/TodoItem.vue'
+
+    export default {
+        name: "app",
+        data() {
+            return {
+                message: 'Hello Vue',
+                groceryList: [{id: 0, text: "蔬菜"}, {id: 1, text: "奶酪"}]
+            }
+        },
+        components: {
+            TodoItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <li class="text">{{todo.text}}</li>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: ['todo'],
+    }
+</script>
+
+<style scoped>
+    .text {
+        color: #42b983;
+    }
+</style>
